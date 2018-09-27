723697b73a5a2c27aa834788d5cbf23395a5c029
Joshua Payette <joshua.payette.2020@student.lhschools.org>
2018-09-27 07:50:47 -0400

    Finished number guessing game

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -5,27 +5,31 @@ public class NumberGuessing {
 
     public static void main(String[]args){
         Scanner board;
-        int pickedNumber, guess;
+        int randomNumber, guess;
 
 
 
         board = new Scanner(System.in);
 
-        pickedNumber = (int)(Math.random()*100);
-        System.out.println(pickedNumber);
+        randomNumber = (int)(Math.random()*100);
         System.out.println("Welcome to the number guessing game. In this game");
         System.out.println("you are going to try and guess the number I am thinking of.");
         System.out.println("Ok start out by guessing a number 1-100");
-        guess = board.nextInt();
-        if (guess == pickedNumber){
-            System.out.println("Your either cheating or ypu need to hit the lottery because YOU WON");
-        }
-        else if (guess < pickedNumber){
-            System.out.println("A little to low for my taste");
-        }
-        else if (guess > pickedNumber){
-            System.out.println("As high as Snoop Dog");
-        }
+
+        do {
+            guess = board.nextInt();
+            if (guess == randomNumber)
+                System.out.println("You're either cheating or you need to hit the lottery because YOU WON");
+
+            else if (guess < randomNumber)
+                System.out.println("A little to low for my taste. Guess again");
+
+            else if (guess > randomNumber)
+                System.out.println("As high as Snoop Dog. Guess again");
+        }while(guess!=randomNumber);
+
+
+
 
 
     }
