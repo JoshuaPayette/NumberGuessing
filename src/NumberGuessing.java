18ec12a46cb0339e88cd6bc77470082bcfeaff26
Joshua Payette <joshua.payette.2020@student.lhschools.org>
2018-10-08 07:58:01 -0400

    Cleaned up and finalized loops

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -5,15 +5,17 @@ public class NumberGuessing {
 
     public static void main(String[]args){
         Scanner board;
-        int randomNumber, guess;
+        int randomNumber,attempts,guess;
         board = new Scanner(System.in);
         String decision;
         do{
         randomNumber = (int)(Math.random()*100);
+            attempts = 0;
 
 
         System.out.println("Welcome to the number guessing game. In this game");
         System.out.println("you are going to try and guess the number I am thinking of.");
+        System.out.println("You have seven attempts so use your guesses wisely.");
         System.out.println("Ok start out by guessing a number 0-100");
 
         do {
@@ -27,8 +29,9 @@ public class NumberGuessing {
 
             else if (guess > randomNumber)
             {System.out.println("As high as Snoop Dog. Guess again");}
-        }while(guess!=randomNumber);
-        System.out.println("would you like to play again?");
+                attempts++;
+        }while(guess!=randomNumber&&attempts<7);
+        System.out.println("Good game, a valiant effort. Would you like to play again?");
         decision = board.nextLine();
         }while(!board.nextLine().equals("no"));
         System.out.println("Thanks for playing!");
